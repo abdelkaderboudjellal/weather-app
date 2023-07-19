aa53d468290e31364da25f47a9553d7adf096ba4
Abdelkader boudjellal <99020132+abdelkaderboudjellal@users.noreply.github.com>
2023-07-19 19:49:05 +0100

    update map

--- components/map.tsx
+++ components/map.tsx
@@ -9,27 +9,31 @@ import { useEffect, useState, useRef } from "react";
 const map = (props: any) => {
   const position: LatLngExpression = [props.lat, props.lon];
   let zoom: number = 13;
-
-  
-  return (
-    <div className="m-0 overflow-hidden h-[380px] w-full">
+  const [localisation, setLocalisation] = useState<LatLngExpression>()
+  const Maps=()=>{
+    return(
       <MapContainer
   
-        style={{ height: "380px", borderRadius: "16px" }}
-        zoom={zoom}
-        center={position}
-        scrollWheelZoom={false}
-        worldCopyJump={false}
-        className="map-container"
-      >
-        <TileLayer
-          noWrap={true}
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>{props.name}</Popup>
-        </Marker>
-      </MapContainer>
+      style={{ height: "380px", borderRadius: "16px" }}
+      zoom={zoom}
+      center={position}
+      scrollWheelZoom={false}
+      worldCopyJump={false}
+      className="map-container"
+    >
+      <TileLayer
+        noWrap={true}
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>{props.name}</Popup>
+      </Marker>
+    </MapContainer>
+    )
+  }
+  return (
+    <div className="m-0 overflow-hidden h-[380px] w-full">
+<Maps/>
     </div>
   );
 };
